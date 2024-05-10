32c0e1065bfd1a2f093f94eb70ed581996859028
Nurda003 <kalmachambet@mail.ru>
2024-05-10 06:16:23 +0500

    done jorunal

--- ds/client/src/pages/Journal.js
+++ ds/client/src/pages/Journal.js
@@ -6,6 +6,8 @@ import cli1 from '../img/clinics1.png'
 import cli2 from '../img/clinics2.png'
 import cli3 from '../img/clinics3.png'
 import Footer from '../comps/Footer'
+import locationI from '../img/location.svg'
+
 
 function Journal() {
     const [bookings, setBookings] = useState([]);
@@ -40,6 +42,15 @@ function Journal() {
     if (isLoading) {
         return <div>Loading...</div>;
     }
+    const getRatingText = (rating) => {
+        if (rating >= 4.5) {
+        return 'Very Good';
+        } else if (rating >= 4) {
+        return 'Good';
+        } else {
+        return 'Normal';
+        }
+    }
 
 
 
@@ -73,18 +84,58 @@ function Journal() {
             
             <div className="flex flex-col w-11/12 justify-center items-center">
 
-
-                <div className="flex w-full  gap-10 p-3 rounded-2xl bg-white items-center mt-10">
+            
+                <div className="flex w-full flex-col  gap-10 p-3 rounded-2xl bg-white items-center mt-10">
+                
                 {error && <p>{error}</p>}
-            {bookings.map((booking, index) => (
-                <div key={index}>
-                    <h2>{booking.clinicId ? booking.clinicId.name : 'Clinic details not available'}</h2>
-                    <p>Date: {booking.date ? new Date(booking.date).toLocaleDateString() : 'Invalid Date'}</p>
-                    <p>Location: {booking.clinicId ? booking.clinicId.location : 'No location available'}</p>
-                    <p>Services: {booking.clinicId && booking.clinicId.services ? booking.clinicId.services.join(', ') : 'No services listed'}</p>
-                    <p>Message: {booking.message}</p>
+                <div className="flex flex-col justify-center items-center py-5">
+                <h1 className="text-4xl text-center font-bold mb-5">My Bookings</h1>
+            {bookings.map((booking) => (
+                <div key={booking._id} className="flex w-full gap-6 p-3 rounded-2xl bg-white items-center mt-10">
+                     
+                    <div className="w-heroimg">
+                        {booking.clinicId?.image?.filename
+                            ? <img src={`/api/image/${booking.clinicId.image.filename}`} alt={booking.clinicId.name} />
+                            : <img src={cli1} alt="Default Clinic" />
+                        }
+                    </div>
+                    <div className="w-full p-3 bg-white">
+                        <div className="flex w-full justify-between">
+                            <div className="flex flex-col gap-3">
+                                <h1 className='text-2xl font-bold text-bigtext'>{booking.clinicId.name}</h1>
+                                <p className='text-base text-smalltext'>
+                                    <span className='flex gap-3'>
+                                        <img src={locationI} alt="" width="25px" />
+                                        {booking.clinicId.address}
+                                    </span>
+                                </p>
+                                <div className="flex flex-col gap-4">
+                                    <p className='font-bold text-bigtext'>Services:</p>
+                                    <div className="flex flex-col w-710 gap-2">
+                                        {booking.clinicId.services && 
+                                            <p className='font-semibold flex'>{booking.clinicId.services.join(' / ')}</p>
+                                        }
+                                    </div>
+                                </div>
+                                <div className="flex gap-4 items-center">
+                                    <p className="bg-white border-blue-600 border w-9 text-bigtext text-sm font-semibold justify-center items-center p-1.5 rounded">
+                                        <span>{booking.clinicId.rating}</span>
+                                    </p>
+                                    <p className='font-semibold'>{getRatingText(booking.clinicId.rating)}</p>
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <p className='text-base text-smalltext font-semibold'>Booked by:</p>
+                                <p className='text-xl text-blue-700 font-semibold max-w-80'>{booking.firstName} {booking.lastName}</p>
+                                <p className='text-base text-smalltext'>On: {new Date(booking.date).toLocaleDateString()}</p>
+                                <p className='text-base text-smalltext'>Message: {booking.message}</p>
+                            </div>
+                        </div>
+                        <hr className='w-full bg-gray-500 my-4' />
+                    </div>
                 </div>
             ))}
+        </div>
                 </div>
                 
                 
